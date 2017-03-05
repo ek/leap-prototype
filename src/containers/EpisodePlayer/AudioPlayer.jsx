ced021b1bc1cea3f6d2bfe8cf159a5c3a6d361c3
Eric Knudtson <ericknudtson@gmail.com>
2017-03-04 16:11:27 -0800

    stuff

---
+++ src/containers/EpisodePlayer/AudioPlayer.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+export default class AudioPlayer extends React.Component {
+
+  componentDidMount() {
+    
+    function getContext() {
+      var contextClass = (window.AudioContext ||  window.webkitAudioContext || 
+          window.mozAudioContext || 
+          window.oAudioContext || 
+          window.msAudioContext);
+      if (contextClass) {
+        // Web Audio API is available.
+        return new contextClass();
+      } else {
+        console.log('Web Audio API is not available.');
+      }
+    }
+
+    function playSound(buffer) {
+      var source = context.createBufferSource();
+      source.buffer = buffer;
+      source.connect(context.destination);
+      source.start(0);
+    }
+
+    function loadAudio(url, context) {
+      var request = new XMLHttpRequest();
+      request.open('GET', url, true);
+      request.responseType = 'arraybuffer';
+      // Decode asynchronously
+      request.onload = function() {
+          context.decodeAudioData(request.response, function(theBuffer) {
+              var buffer = theBuffer;
+              playSound(buffer);
+          }, function(err) {
+              console.log(err);
+          });
+      }
+      request.send();
+    }
+    const context = getContext();
+    console.log(context)
+    const { audioUrl } = this.props;
+    loadAudio(audioUrl, context);
+
+  }
+
+  render() {
+    const { audioUrl } = this.props;
+    return (
+      <h3>
+        {audioUrl}
+      </h3>
+    )
+  }
+}
+
+
+
+
--- src/containers/EpisodePlayer/index.js
+++ src/containers/EpisodePlayer/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import AudioPlayer from './AudioPlayer.jsx';
 import './index.css';
 
 export default class EpisodePlayer extends React.Component {
   render() {
-    const { setPage } = this.props;
+    const { setPage, audioUrl } = this.props;
+
+    const url = '/audio/the-daily.mp3';
 
     return (
       <div className='player-view'>
         <button onClick={() => setPage('overview')}>Back</button>
         Player View
+        <AudioPlayer audioUrl={url}></AudioPlayer>
       </div>
     )
   }
