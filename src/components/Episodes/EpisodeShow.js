ca319ff9a878d1db93e69395519d63f325ec0b21
Eric Knudtson <ericknudtson@gmail.com>
2017-03-07 22:58:22 -0800

    episode show page starter

--- src/components/Episodes/EpisodeShow.js
+++ src/components/Episodes/EpisodeShow.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import Header from '../shared/Header';
+
+import './EpisodeShow.css';
 
 export default class EpisodeShow extends React.Component {
+  
   render() {
-    const { setPage, title, description } = this.props;
+    const { setView } = this.props.setView;
+    const { title, transcript } = this.props.episode;
     return (
-      <div className="content">
-        {title}
-        <button onClick={() => setPage('player')}>Play</button>
+      <div>
+        <Header
+          setView={setView}
+          title={title}
+          previousView='EpisodeIndex' />
+        <div className='content'>
+          <ul className="table-view">
+            {this.renderSentences(transcript)}
+          </ul>
+        </div>
       </div>
     )
   }
+
+  renderSentences(transcript) {
+    return transcript.map((sentence, index) => {
+      return (
+        <li key={index} className="table-view-cell sentence-cell">
+          <a className="">
+            {sentence.text}
+          </a>
+        </li>
+      )
+    });
+  }
+
 }
