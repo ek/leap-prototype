9537d1b5b7be669cae42158b0339c41331c1b656
Eric Knudtson <ericknudtson@gmail.com>
2017-03-14 00:17:37 -0700

    adding podcast placeholders

--- src/data/podcasts/index.js
+++ src/data/podcasts/index.js
@@ -1,5 +1,145 @@
 import TheDaily from './the-daily';
 
 export default [
-  TheDaily
+  TheDaily,
+  {
+    key: 2,
+    title: 'Crimetown',
+    author: 'Gimlet Media',
+    image: 'images/crimetown.jpg',
+    description: '',
+    episodes: []
+  },
+  {
+    key: 3,
+    title: 'EMBD',
+    author: 'Embedded',
+    image: 'images/embd.jpg',
+    description: '',
+    episodes: []
+  },
+  {
+    key: 4,
+    title: 'Politics and More',
+    author: 'WNYC Studios and The New Yorker',
+    image: 'images/politics-and-more.jpg',
+    description: '',
+    episodes: []
+  },
+  {
+    key: 5,
+    title: 'With Friends Like These',
+    author: 'Crooked Media',
+    image: 'images/with-friends-like-these.jpg',
+    description: '',
+    episodes: []
+  },
+  {
+    key: 6,
+    title: 'Pod Save The World',
+    author: 'Crooked Media',
+    image: 'images/pod-save-the-world.jpg',
+    description: '',
+    episodes: []
+  },
+  {
+    key: 7,
+    title: 'UpAndVanished',
+    author: 'Tenderfoot',
+    image: 'images/up-and-vanished.jpg',
+    description: '',
+    episodes: []
+  },
+  {
+    key: 8,
+    title: 'The45th',
+    author: 'AudioBoom',
+    image: 'images/the-45th.jpg',
+    description: '',
+    episodes: []
+  },
+  {
+    key: 9,
+    title: 'Serial',
+    author: 'This American Life',
+    image: 'images/serial.jpg',
+    description: '',
+    episodes: []
+  },
+  {
+    key: 10,
+    title: 'Rachel Maddow',
+    author: 'MSNBC',
+    image: 'images/rachel-maddow.jpg',
+    description: '',
+    episodes: []
+  },
+  {
+    key: 11,
+    title: 'Pod Save America',
+    author: 'Crooked Media',
+    image: 'images/pod-save-america.jpg',
+    description: '',
+    episodes: []
+  },
+
+  {
+    key: 12,
+    title: '60 Minutes',
+    author: 'Play.it',
+    image: 'images/60-minutes.jpg',
+    description: '',
+    episodes: []
+  },
+
+  {
+    key: 13,
+    title: 'Undone',
+    author: 'Gimlet Media',
+    image: 'images/undone.jpg',
+    description: '',
+    episodes: []
+  },
+
+  {
+    key: 14,
+    title: 'The Economist Radio',
+    author: 'The Economist',
+    image: 'images/the-economist.jpg',
+    description: '',
+    episodes: []
+  },
+
+  {
+    key: 15,
+    title: 'Slate Political Gabfest',
+    author: 'Slate Magazine',
+    image: 'images/slate-political-gabfest.jpg',
+    description: '',
+    episodes: []
+  },
+
 ];
+
+/*
+import 60Minutes from './60-minutes';
+import Accused from './accused';
+import AndersonCooper from './anderson-cooper';
+import BBCWolrdService from './bbc-wolrd-service';
+import Civics101 from './civics-101';
+import CommonSense from './common-sense';
+import Attn from './attn';
+import EzraKlein from './ezra-klein';
+import FiveThirtyEight from './five-thirty-eight';
+import FoxNews from './fox-news';
+import GlobalPolitico from './global-politico';
+import Indivisible from './indivisible';
+import InsideTheTimes from './inside-the-times';
+import Intercepted from './intercepted';
+import Longform from './longform';
+import MeetThePress from './meet-the-press';
+import Nerdcast from './nerdcast';
+import NPRPolitics from './npr-politics';
+import OnTheMedia from './on-the-media';
+import PBSNewsHour from './pbs-news-hour';
+*/
